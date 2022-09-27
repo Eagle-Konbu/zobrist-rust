c6110e68facb0596d9cf6c47e867fb4f7e0f98ff
Atsuya Uchida <atsuya_eagle@outlook.com>
2022-09-27 17:30:23 +0900

    Solve ABC250 E using zobrist.

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,86 @@
+use proconio::input;
 use rand::prelude::*;
+use std::collections::{HashMap, HashSet};
 
 fn main() {
-    let zobrist = Zobrist::new(vec![1, 2, 3, 4]);
-    println!("{:?}", zobrist);
+    input! {
+        n: usize,
+        a: [i64; n],
+        b: [i64; n],
+        q: usize
+    };
+
+    let mut items = a.clone();
+    items.extend(b.clone());
+
+    items.sort_unstable();
+    items.dedup();
+
+    let zobrist = Zobrist::new(items);
+
+    let mut hash_a = vec![0; n];
+    let mut set_a: HashSet<i64> = std::collections::HashSet::new();
+    for (i, &a_item) in a.iter().enumerate() {
+        if set_a.contains(&a_item) {
+            if i > 0 {
+                hash_a[i] = hash_a[i - 1];
+            }
+        } else {
+            set_a.insert(a_item);
+            if i > 0 {
+                hash_a[i] = hash_a[i - 1] ^ zobrist.hash_table.get(&a_item).unwrap();
+            } else {
+                let &item_hash = zobrist.hash_table.get(&a_item).unwrap();
+                hash_a[i] = item_hash;
+            }
+        }
+    }
+
+    let mut hash_b = vec![0; n];
+    let mut set_b: HashSet<i64> = std::collections::HashSet::new();
+    for (i, &b_item) in b.iter().enumerate() {
+        if set_b.contains(&b_item) {
+            if i > 0 {
+                hash_b[i] = hash_b[i - 1];
+            }
+        } else {
+            set_b.insert(b_item);
+            if i > 0 {
+                hash_b[i] = hash_b[i - 1] ^ zobrist.hash_table.get(&b_item).unwrap();
+            } else {
+                let &item_hash = zobrist.hash_table.get(&b_item).unwrap();
+                hash_b[i] = item_hash;
+            }
+        }
+    }
+
+    for _ in 0..q {
+        input! {
+            x: usize,
+            y: usize
+        };
+        if hash_a[x - 1] == hash_b[y - 1] {
+            println!("Yes");
+        } else {
+            println!("No");
+        }
+    }
 }
 
 #[derive(Debug, Clone)]
-struct Zobrist<T> {
+struct Zobrist<T: std::hash::Hash + std::cmp::Eq + Copy> {
     items: Vec<T>,
-    hashes: Vec<u64>,
+    hash_table: HashMap<T, u64>,
 }
 
-impl<T> Zobrist<T> {
+impl<T: std::hash::Hash + std::cmp::Eq + Copy> Zobrist<T> {
     fn new(items: Vec<T>) -> Zobrist<T> {
         let mut rng = rand::rngs::StdRng::seed_from_u64(0);
 
-        let hashes = (0..items.len())
-            .map(|_| rng.gen::<u64>())
-            .collect::<Vec<u64>>();
-        Zobrist { items, hashes }
-    }
-
-    fn set_hash(&self, indexes: Vec<usize>) -> u64 {
-        let mut res = 0;
-        for i in indexes {
-            res ^= self.hashes[i];
+        let mut hash_table = HashMap::new();
+        for &item in items.iter() {
+            hash_table.insert(item, rng.gen::<u64>());
         }
-
-        res
-    }
-
-    fn append_item_to_set_hash(&self, set: u64, item: usize) -> u64 {
-        set ^ self.hashes[item]
+        Zobrist { items, hash_table }
     }
 }
