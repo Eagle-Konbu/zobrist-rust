a68ba6cd92609b1db06a4ec29f3d0c6e346fa316
Atsuya Uchida <atsuya_eagle@outlook.com>
2022-09-27 16:17:48 +0900

    Use stdrng to define seed.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use rand::Rng;
+use rand::prelude::*;
 
 fn main() {
     let zobrist = Zobrist::new(vec![1, 2, 3, 4]);
@@ -8,14 +8,16 @@ fn main() {
 #[derive(Debug, Clone)]
 struct Zobrist<T> {
     items: Vec<T>,
-    hashes: Vec<u64>
+    hashes: Vec<u64>,
 }
 
 impl<T> Zobrist<T> {
     fn new(items: Vec<T>) -> Zobrist<T> {
-        let mut rng = rand::thread_rng();
+        let mut rng = rand::rngs::StdRng::seed_from_u64(0);
 
-        let hashes = (0..items.len()).map(|_| rng.gen::<u64>()).collect::<Vec<u64>>();
+        let hashes = (0..items.len())
+            .map(|_| rng.gen::<u64>())
+            .collect::<Vec<u64>>();
         Zobrist { items, hashes }
     }
 }
